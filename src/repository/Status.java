402a4bccfd8c04b815293b7dc12b5015836fdd8a
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-08-11 18:39:04 +0530

    added helper method shouldIgnore in Status.java

--- src/repository/Status.java
+++ src/repository/Status.java
@@ -9,34 +9,34 @@ public class Status {
         scan(".");
 
     }
-    static void scan(String directroy){
+   private static void scan(String directroy){
 
         File current = new File(directroy);
         File[] files = current.listFiles();
         if(current.isDirectory()){
             if(files == null){
                 System.out.println("Unable to read directory");
+                return;
             }
 
             for (File file  : files) {
-                if (files == null) {
-                    System.out.println("Unable to read directory");
-                    return;
-                }
-                if(file.equals(".jgit"))
+                if(shouldIgnore(file))
                     continue;
 
                 if(file.isFile())
                     System.out.println("File : "+file.getName());
 
-                if(file.isDirectory())
+                if(file.isDirectory()) {
                     System.out.println("Dir : " + file.getName());
-
-
-                scan(file.toString());
+                    scan(file.toString());
+                }
             }
         }
 
+    }
 
+    private static boolean shouldIgnore(File file){
+        if(file.getName().equals(".jgit") || file.getName().endsWith(".class"))return true;
+        return false;
     }
 }
